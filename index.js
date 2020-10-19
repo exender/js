21a0e27c2ad29bc0a5eb1e43c4c3060ccd997835
exender <baptistebonnin4@gmail.com>
2020-10-19 16:55:28 +0200

    modif

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const axios = require("axios");
 
-const fetchTracks = (searchTerm) => {
-    axios.get("https://api.deezer.com/search?q=discovery")
-    .then(response => console.log(response.data));
+const printTracks = (tracks) => {
+    for (let i = 0; i < tracks.length; i++) {
+        const track = tracks[i];
+        console.log(track.title);
+    }
 };
 
-fetchTracks("discovery");
+const fetchTracks = (searchTerm) => {
+    axios
+        .get("https://api.deezer.com/search?q=" + searchTerm)
+        .then((response) => printTracks(response.data.data));
+};
+
+fetchTracks("nolwenn leroy");
